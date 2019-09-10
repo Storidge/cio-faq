1d7fd8ad1c5ec636f75015c4d6a3589e83b48bea
jontkong <jkong@storidge>
2019-09-09 21:19:44 -0700

    Add sitemap

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -9,21 +9,26 @@ module.exports = {
         {
           'ga': 'UA-102974094-1' // UA-00000000-0
         }
-      ]
+      ],
+      {
+        'sitemap': {
+          hostname: 'https://pake.web.id'
+        },
+      }
     ],
     themeConfig: {
 //        logo: 'https://storidge.com/wp-content/uploads/2019/04/logo_storidge_automated_2_370x100.png',
-        lastUpdated: 'Last Updated',
-        editLinks: true,
-        editLinkText: 'Help us improve this page!',
-        sidebarDepth: 3,
-        nav: [
-            { text: 'Home', link: 'https://storidge.com', },
-            { text: 'Guide', link: 'https://guide.storidge.com' },
-            { text: 'Docs', link: 'https://docs.storidge.com' },
-            { text: 'API', link: 'https://storidge.com/api' },
-            { text: 'Support', link: 'https://storidge.com/support' }
-        ],
+      lastUpdated: 'Last Updated',
+      editLink: true,
+      editLinkText: 'Help us improve this page!',
+      sidebarDepth: 3,
+      nav: [
+          { text: 'Home', link: 'https://storidge.com', },
+          { text: 'Guide', link: 'https://guide.storidge.com' },
+          { text: 'Docs', link: 'https://docs.storidge.com' },
+          { text: 'API', link: 'https://storidge.com/api' },
+          { text: 'Support', link: 'https://storidge.com/support' }
+      ],
         sidebar: [
           ['/', 'FAQ Home'],
           {
